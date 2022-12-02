71d884045c7651bb639cf3f10d7036ceeecdd2be
evgenyvin0kurov <112849308+evgenyvin0kurov@users.noreply.github.com>
2022-12-02 21:27:26 +0300

    Add files via upload

---
+++ D4.7/newapp/views.py
@@ -0,0 +1,85 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Author
+from datetime import datetime
+from .filters import PostFilter
+from .forms import PostForm
+from django.urls import reverse_lazy
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_publication'] = None
+        return context
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — post.html
+    template_name = 'post.html'
+    # Название объекта, в котором будет выбранный пользователем post
+    context_object_name = 'post'
+
+class PostSearch(ListView):
+    model = Post
+    ordering = '-dateCreation'
+    template_name = 'search.html'
+    context_object_name = 'news'
+    paginate_by = 10
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+class PostCreate(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.categoryType = 'NW'
+        post.dateCreation = datetime.utcnow()
+        # post.author = Author.objects.get(self.request.user)
+        return super().form_valid(form)
+
+
+class PostUpdate(UpdateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'post_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.categoryType = 'NW'
+        post.dateCreation = datetime.utcnow()
+        # post.author = Author.objects.get(self.request.user)
+        return super().form_valid(form)
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post_list')
